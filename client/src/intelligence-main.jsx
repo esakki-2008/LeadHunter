e4916789a57fb90595cd9becbfc5ea18a87e7173
ESAKKI RAJA SALAIKUMAR <esakkiraja2528@gmail.com>
2026-09-17 17:10:36 +0530

    feat: mount intelligence center

---
+++ client/src/intelligence-main.jsx
@@ -0,0 +1,1 @@
+import React from'react';import{createRoot}from'react-dom/client';import IntelligenceCenter from'./IntelligenceCenter';createRoot(document.getElementById('root')).render(<IntelligenceCenter/>);
