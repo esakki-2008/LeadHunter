6bb0140a5a8fe9a2bb4be66910483d5c8cbca4b7
ESAKKI RAJA SALAIKUMAR <esakkiraja2528@gmail.com>
2026-09-17 17:15:24 +0530

    feat: mount action queue

---
+++ client/src/action-main.jsx
@@ -0,0 +1,1 @@
+import React from'react';import{createRoot}from'react-dom/client';import ActionQueue from'./ActionQueue';import'./dark-theme.css';createRoot(document.getElementById('root')).render(<ActionQueue/>);
