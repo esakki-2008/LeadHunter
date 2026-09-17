99ac59ae22ab18a3e4860d34aa368319110aa320
ESAKKI RAJA SALAIKUMAR <esakkiraja2528@gmail.com>
2026-09-17 16:09:52 +0530

    fix: activate campaign lead filters

--- client/src/campaigns-main.jsx
+++ client/src/campaigns-main.jsx
@@ -1,1 +1,1 @@
-import React from'react';import{createRoot}from'react-dom/client';import Campaigns from'./Campaigns';createRoot(document.getElementById('root')).render(<Campaigns/>);
+import React from'react';import{createRoot}from'react-dom/client';import CampaignsPro from'./CampaignsPro';createRoot(document.getElementById('root')).render(<CampaignsPro/>);
